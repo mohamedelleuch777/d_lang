b84265c3b493c4da23b45fe2003b1aba64f7348b
mohamedelleuch777 <usher7med@gmail.com>
2022-01-23 07:14:18 +0300

    mac using c

--- macos/main.asm
+++ macos/main.asm
@@ -4,14 +4,10 @@
 section .data
     msg db "this message is from asm", 10, 0
     text db "Hello, World! hahaha", 10, 0
-    num dd 1234567890
-    stringFormat db "s", 0
-    integerFormat db "i", 0
-    floatFormat db "f", 0
+    num dq 1234567890
     stringPtr dq 0 ; 64bit size pointer
     ; var int_1 integer
     int_1 dq 48
-    CR db 10, 0
 
 
 section .bss
@@ -22,16 +18,16 @@ section .bss
     
  PROGRAM_START
     
-    write stringFormat, text
-    write integerFormat, 67832
+    write 's', text
+    ;writeln 'i', 67832
     malloc testPtr, 4096
     malloc boufa, 4096
-    read testPtr, 4096
+    read num, 4096
 
-    memset boufa, 0x0, 4096
-    memcpy boufa, testPtr, 3
+    ;memset boufa, 0x0, 4096
+    memcpy boufa, num, 3
 
-    write stringFormat, boufa
+    ;writeln 'a', num
 
 
 
