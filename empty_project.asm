52e7e72c412aa29e75b7e1da7eeda265c5faeac0
mohamedelleuch777 <usher7med@gmail.com>
2022-02-04 19:48:26 +0300

    created min working program

---
+++ empty_project.asm
@@ -0,0 +1,29 @@
+%ifidn __OUTPUT_FORMAT__, win64
+
+%elifidn __OUTPUT_FORMAT__, elf64
+    %include "../inc/xilyor.inc"
+%elifidn __OUTPUT_FORMAT__, macho64
+    %include "xilyor.inc"
+%endif
+
+extern printf
+extern malloc
+
+
+section .data
+
+
+
+section .bss
+
+
+;RDI, RSI, RDX, RCX, R8, and R9 for the first 6 arguments
+    
+ PROGRAM_START
+    
+    
+
+
+
+PROGRAM_END 0
+    
