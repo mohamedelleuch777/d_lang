593b6121d5649df5311ddd2717a4d360186470b4
mohamedelleuch777 <usher7med@gmail.com>
2022-02-06 10:03:37 +0300

    sdl working on mac

--- sdl.asm
+++ sdl.asm
@@ -6,42 +6,73 @@
     %include "xilyor.inc"
 %endif
 
-extern _SDL_Init
-extern _mySDL_Init
-extern _write
+
 
 section .data
 
-    text db "Hello, World! hahaha", 10, 0
-    format db "%s", 0
+    window_title        db "Hello, World! hahaha", 10, 0
+    err1                db "Cannot initialize the Framework", 10, 0
+    err2                db "Sdl was initialized but it couldn't create a window", 10, 0
+    format              db "%s", 0
 
+    isRunning           dq 0x1
 
 section .bss
-
+screen              resq        1 ; screen pointer 1 qword
+SDL_Event           resb        56
 
 ;RDI, RSI, RDX, RCX, R8, and R9 for the first 6 arguments
     
  PROGRAM_START
     
 
-
+    ;push 0x00000020
+    mov rdi, 0x00000020
+    call SDL_Init
+    cmp rax, 0
+    jge  sdl_was_initialized
     
     ; printf
     mov     qword rdi, format
-    mov     qword rsi, text
-    call    _write
-
-
-push 0xFFFF
-mov rdi, 0xFFFF
-call _SDL_Init
-    
-    ; printf
-    mov     qword rdi, format
-    mov     qword rsi, text
+    mov     qword rsi, err1
     call    c_printf
+    PROGRAM_END 1 ; exit with error code 1
+
+    sdl_was_initialized:
+
+    mov rdi, window_title
+    mov rsi, 0x1FFF0000 ; SDL_WINDOWPOS_UNDEFINED
+    mov rdx, 0x1FFF0000 ; SDL_WINDOWPOS_UNDEFINED
+    mov rcx, 640
+    mov r8, 480
+    mov r9, 0x00000002 ; SDL_WINDOW_OPENGL
+    call SDL_CreateWindow
+    mov [rel screen], rax
+    cmp rax, 0
+    jne  window_was_created
+    
+    ; printf
+    mov     qword rdi, format
+    mov     qword rsi, err2
+    call    c_printf
+    call    SDL_Quit
+    PROGRAM_END 2 ; exit with error code 2
+
+    window_was_created:
+
+    while:
+    cmp qword [rel isRunning], 0
+    je  exit_program
+
+    call SDL_PollEvent
+    mov [rel SDL_Event], rax
 
 
 
+
+    jmp while
+
+    exit_program:
+call    SDL_Quit
 PROGRAM_END 0
     
--- xilyor/xilyor.c
+++ xilyor/xilyor.c
@@ -1,43 +1,41 @@
 #include "stdio.c"
 #include "memory.c"
-#include <SDL2/SDL.h>
-#undef main
 
-int mySDL_Init() {
+// int mySDL_Init() {
 
-    SDL_Window *screen;
+//     SDL_Window *screen;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        return 1;
+//     if (SDL_Init(SDL_INIT_VIDEO) < 0)
+//         return 1;
 
-    screen = SDL_CreateWindow("My Game Window",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              640, 480,
-                              SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
-    if (!screen) {
-        SDL_Quit();
-        return 1;
-    }
+//     screen = SDL_CreateWindow("My Game Window",
+//                               SDL_WINDOWPOS_UNDEFINED,
+//                               SDL_WINDOWPOS_UNDEFINED,
+//                               640, 480,
+//                               SDL_WINDOW_OPENGL);
+//     if (!screen) {
+//         SDL_Quit();
+//         return 1;
+//     }
 
-    SDL_Event event;
-    int running = 1;
+//     SDL_Event event;
+//     int running = 1;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                running = 0;
-                break;
-            case SDL_KEYDOWN:
-                running = 0;
-                break;
-            default:
-                break;
-            }
-        }
-    }
+//     while (running) {
+//         while (SDL_PollEvent(&event)) {
+//             switch (event.type) {
+//             case SDL_QUIT:
+//                 running = 0;
+//                 break;
+//             case SDL_KEYDOWN:
+//                 running = 0;
+//                 break;
+//             default:
+//                 break;
+//             }
+//         }
+//     }
 
-    SDL_Quit();
-    return 0;
-}
+//     SDL_Quit();
+//     return 0;
+// }
